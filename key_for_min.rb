686d0b690360657ec0b11c15ea6af67ef8d8a968
enchanting-page-2316 <yannick.hoegerle@gmail.com>
2018-12-31 14:39:12 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowest_pair = ["nil", Float::INFINITY]
+  if name_hash.empty? == true
+    nil
+  else
+    name_hash.each do |key, val|
+      if val < lowest_pair[1]
+        lowest_pair[1] = val and lowest_pair[0] = key
+      end
+    end
+    lowest_pair[0]
+  end
 end
