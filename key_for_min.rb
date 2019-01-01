6978b6f44952a5846012c18895c72d8b071420a5
parik panchal <parikspanchal@icloud.com>
2019-01-01 19:17:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,27 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
 
+def key_for_min_value(hash)
+zarray = []
+hash.each do |key, value|
+  value = "#{value}"
+	zarray.push(value.to_i)
+end
+def bubble_sort(zarray)
+  return array if array.size <= 1
+  swap = true
+    while swap
+      swap = false
+      (array.length - 1).times do |x|
+        if array[x] > array[x+1]
+          array[x], array[x+1] = array[x+1], array[x]
+          swap = true
+        end
+      end
+    end
+  array
+end
+corr = zarray[0]
+hash.index(corr)
 end
